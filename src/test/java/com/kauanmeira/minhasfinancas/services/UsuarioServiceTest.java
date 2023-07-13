b6b8d6370521de170b805ee055eae84924bffda6
kauan.meira <kauangabriel.meira123@gmail.com>
2023-07-13 20:56:43 -0300

    Adicionando Mocks nos testes de UsuarioService

--- src/test/java/com/kauanmeira/minhasfinancas/services/UsuarioServiceTest.java
+++ src/test/java/com/kauanmeira/minhasfinancas/services/UsuarioServiceTest.java
@@ -2,50 +2,53 @@ package com.kauanmeira.minhasfinancas.services;
 
 import com.kauanmeira.minhasfinancas.exceptions.RegraNegocioException;
 import com.kauanmeira.minhasfinancas.model.entities.Usuario;
+import com.kauanmeira.minhasfinancas.model.impl.UsuarioServiceImpl;
 import com.kauanmeira.minhasfinancas.model.repositories.UsuarioRepository;
 import com.kauanmeira.minhasfinancas.model.services.UsuarioService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
-    @Autowired
-    UsuarioService service;
 
     @Autowired
-    UsuarioRepository repository;
+    private UsuarioService service;
+
+    @MockBean
+    private UsuarioRepository repository;
+
+    @BeforeEach
+    public void setUp(){
+        service = new UsuarioServiceImpl(repository);
+    }
 
     @Test
     public void deveValidarEmail() {
-        assertDoesNotThrow(() -> {
-            //cenario
-            repository.deleteAll();
+        // cenário
+        when(repository.existsByEmail(any(String.class))).thenReturn(false);
 
-            //acao
-            service.validarEmail("email@email.com");
-        });
+        // ação e verificação
+        assertDoesNotThrow(() -> service.validarEmail("email@example.com"));
     }
 
     @Test
     public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
-        assertThrows(RegraNegocioException.class, () -> {
-            //cenario
-            Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-            repository.save(usuario);
+        // cenário
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
-            //acao
-            service.validarEmail("email@email.com");
-
-        });
-
+        // ação e verificação
+        assertThrows(RegraNegocioException.class, () -> service.validarEmail("email@example.com"));
     }
 }
