f2f439147042d0841f9491adf4efe1efa0cc931f
kauan.meira <kauangabriel.meira123@gmail.com>
2023-07-14 20:09:32 -0300

    Implementando Endpoint de autenticação de Usuários

--- src/main/java/com/kauanmeira/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/kauanmeira/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,6 +1,7 @@
 package com.kauanmeira.minhasfinancas.api.resource;
 
 import com.kauanmeira.minhasfinancas.api.dto.UsuarioDTO;
+import com.kauanmeira.minhasfinancas.exceptions.ErroAutenticacao;
 import com.kauanmeira.minhasfinancas.exceptions.RegraNegocioException;
 import com.kauanmeira.minhasfinancas.model.entities.Usuario;
 import com.kauanmeira.minhasfinancas.model.services.UsuarioService;
@@ -12,21 +13,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/usuarios")
 public class UsuarioResource {
 
-   private UsuarioService service;
+    private UsuarioService service;
 
-   public UsuarioResource(UsuarioService service){
-      this.service = service;
-   }
-   @PostMapping
-   public ResponseEntity salvar(@RequestBody UsuarioDTO dto){
+    public UsuarioResource(UsuarioService service) {
+        this.service = service;
+    }
 
-      Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
 
-      try{
-         Usuario usuarioSalvo = service.salvarUsuario(usuario);
-         return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
-      }catch (RegraNegocioException e){
-         return ResponseEntity.badRequest().body(e.getMessage());
-      }
-   }
+        Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
+
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/autenticar")
+    public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+        try {
+            Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        } catch (ErroAutenticacao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
